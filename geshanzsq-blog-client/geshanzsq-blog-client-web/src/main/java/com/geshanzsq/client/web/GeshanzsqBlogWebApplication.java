0e203342b1b04cc16e3298334b292e0750ea43ce
geshanzsq <497301391@qq.com>
2022-09-15 21:17:01 +0800

    feat：调整模块目录结构

--- geshanzsq-blog-client/geshanzsq-blog-client-web/src/main/java/com/geshanzsq/client/web/GeshanzsqBlogWebApplication.java
+++ geshanzsq-blog-client/geshanzsq-blog-client-web/src/main/java/com/geshanzsq/client/web/GeshanzsqBlogWebApplication.java
@@ -1,4 +1,4 @@
-package com.geshanzsq.front;
+package com.geshanzsq.client.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
