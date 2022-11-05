d8c0c5c0e59b09274a7fdaf27de35006af5e5bfb
geshanzsq <497301391@qq.com>
2022-11-05 19:16:28 +0800

    feat(MyBatisPlus构造器): 扩展 LambdaQueryWrapper 类，重写部分方法

--- geshanzsq-blog-common/geshanzsq-blog-common-framework/src/main/java/com/geshanzsq/common/framework/mybatis/plugin/query/LambdaQueryWrapperPlus.java
+++ geshanzsq-blog-common/geshanzsq-blog-common-framework/src/main/java/com/geshanzsq/common/framework/mybatis/plugin/query/LambdaQueryWrapperPlus.java
@@ -20,83 +20,96 @@ public class LambdaQueryWrapperPlus<T> extends LambdaQueryWrapper<T> {
      * 模糊查询
      */
     public LambdaQueryWrapperPlus<T> likeIf(SFunction<T, ?> column, String value) {
-        if (StrUtils.isNotBlank(value)) {
-            return (LambdaQueryWrapperPlus<T>) super.like(column, value);
-        }
-        return this;
+        return StrUtils.isBlank(value) ? this : like(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> like(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus) super.like(column, value);
     }
 
     /**
      * 相等
      */
     public LambdaQueryWrapperPlus<T> eqIf(SFunction<T, ?> column, Object value) {
-        if (value == null) {
-            return this;
-        }
-        if (value instanceof String && StrUtils.isBlank(String.valueOf(value))) {
-            return this;
-        }
-        return (LambdaQueryWrapperPlus<T>) super.eq(column, value);
+        return StrUtils.isNullBlank(value) ? this : eq(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> eq(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus) super.eq(column, value);
     }
 
     /**
      * 包含
      */
     public LambdaQueryWrapperPlus<T> inIf(SFunction<T, ?> column, Collection<?> values) {
-        if (CollectionUtils.isEmpty(values)) {
-            return this;
-        }
-        return (LambdaQueryWrapperPlus<T>) super.in(column, values);
+        return CollectionUtils.isEmpty(values) ? this : in(column, values);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> in(SFunction<T, ?> column, Collection<?> values) {
+        return (LambdaQueryWrapperPlus) super.in(column, values);
     }
 
     /**
      * 不包含
      */
     public LambdaQueryWrapperPlus<T> notInIf(SFunction<T, ?> column, Collection<?> values) {
-        if (CollectionUtils.isEmpty(values)) {
-            return this;
-        }
-        return (LambdaQueryWrapperPlus<T>) super.notIn(column, values);
+        return CollectionUtils.isEmpty(values) ? this : notIn(column, values);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> notIn(SFunction<T, ?> column, Collection<?> values) {
+        return (LambdaQueryWrapperPlus) super.notIn(column, values);
     }
 
     /**
      * 小于
      */
     public LambdaQueryWrapperPlus<T> ltIf(SFunction<T, ?> column, Object value) {
-        if (value != null) {
-            return (LambdaQueryWrapperPlus<T>) super.lt(column, value);
-        }
-        return this;
+        return StrUtils.isNullBlank(value) ? this : lt(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> lt(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus) super.lt(column, value);
     }
 
     /**
      * 小于等于
      */
     public LambdaQueryWrapperPlus<T> leIf(SFunction<T, ?> column, Object value) {
-        if (value != null) {
-            return (LambdaQueryWrapperPlus<T>) super.le(column, value);
-        }
-        return this;
+        return StrUtils.isNullBlank(value) ? this : le(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> le(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus) super.le(column, value);
     }
 
     /**
      * 大于
      */
     public LambdaQueryWrapperPlus<T> gtIf(SFunction<T, ?> column, Object value) {
-        if (value != null) {
-            return (LambdaQueryWrapperPlus<T>) super.gt(column, value);
-        }
-        return this;
+        return StrUtils.isNullBlank(value) ? this : gt(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> gt(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus) super.gt(column, value);
     }
 
     /**
      * 大于等于
      */
     public LambdaQueryWrapperPlus<T> geIf(SFunction<T, ?> column, Object value) {
-        if (value != null) {
-            return (LambdaQueryWrapperPlus<T>) super.ge(column, value);
-        }
-        return this;
+        return StrUtils.isNullBlank(value) ? this : ge(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> ge(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus) super.ge(column, value);
     }
 
 }
