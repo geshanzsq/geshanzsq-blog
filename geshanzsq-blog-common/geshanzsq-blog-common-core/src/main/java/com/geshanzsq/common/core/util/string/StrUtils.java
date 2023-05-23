9c9150603313c6cf6f0c64140d1f95160c13b924
geshanzsq <497301391@qq.com>
2023-05-23 22:38:43 +0800

    fix(xss): 修复 XSS 漏洞

--- geshanzsq-blog-common/geshanzsq-blog-common-core/src/main/java/com/geshanzsq/common/core/util/string/StrUtils.java
+++ geshanzsq-blog-common/geshanzsq-blog-common-core/src/main/java/com/geshanzsq/common/core/util/string/StrUtils.java
@@ -2,9 +2,11 @@ package com.geshanzsq.common.core.util.string;
 
 import com.geshanzsq.common.core.constant.CommonConstant;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.CollectionUtils;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * 字符串工具类
@@ -90,6 +92,40 @@ public class StrUtils extends StringUtils {
      */
     public static boolean isNotNullBlank(Object value) {
         return !isNullBlank(value);
+    }
+
+    /**
+     * 查找指定字符串是否匹配指定字符串列表中的任意一个字符串
+     *
+     * @param str     指定字符串
+     * @param strList 需要检查的字符串数组
+     * @return 是否匹配
+     */
+    public static boolean matches(String str, List<String> strList) {
+        if (CollectionUtils.isEmpty(strList)) {
+            return false;
+        }
+        for (String pattern : strList) {
+            if (isMatch(pattern, str)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断url是否与规则配置:
+     * ? 表示单个字符;
+     * * 表示一层路径内的任意字符串，不可跨层级;
+     * ** 表示任意层路径;
+     *
+     * @param pattern 匹配规则
+     * @param url     需要匹配的url
+     * @return
+     */
+    public static boolean isMatch(String pattern, String url) {
+        AntPathMatcher matcher = new AntPathMatcher();
+        return matcher.match(pattern, url);
     }
 
 }
